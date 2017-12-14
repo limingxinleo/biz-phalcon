533e064777af3daf8d77b7d9c57b6e1e064b34b2
李铭昕 <715557344@qq.com>
2017-12-14 18:19:51 +0800

    增加新建Gate脚本

--- app/tasks/Make/GateTask.php
+++ app/tasks/Make/GateTask.php
@@ -3,13 +3,69 @@
 namespace App\Tasks\Make;
 
 use App\Tasks\Task;
+use Phalcon\Text;
+use Xin\Cli\Color;
+use Xin\Phalcon\Cli\Traits\Input;
 
 class GateTask extends Task
 {
+    use Input;
 
     public function mainAction()
     {
+        echo Color::head('Help:') . PHP_EOL;
+        echo Color::colorize('  新建Gate层脚本') . PHP_EOL . PHP_EOL;
 
+        echo Color::head('Usage:') . PHP_EOL;
+        echo Color::colorize('  php run make:gate@[action] class=className --force', Color::FG_LIGHT_GREEN) . PHP_EOL . PHP_EOL;
+
+        echo Color::head('Actions:') . PHP_EOL;
+        echo Color::colorize('  request         新建Request', Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::colorize('  validator       新建Validator', Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::colorize('  business        新建Business', Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::colorize('  response        新建Response', Color::FG_LIGHT_GREEN) . PHP_EOL;
+        echo Color::colorize('  all             新建所有', Color::FG_LIGHT_GREEN) . PHP_EOL;
+
+    }
+
+    public function __call($name, $arguments)
+    {
+        $type = str_replace('Action', '', $name);
+        if ($type === 'all') {
+            $this->execute('Request');
+            $this->execute('Validator');
+            $this->execute('Business');
+            $this->execute('Response');
+        } else {
+            $this->execute(Text::camelize($type));
+        }
+    }
+
+    public function execute($type)
+    {
+        $class = $this->argument('class');
+        $arr = explode('\\', $class);
+        $class = array_pop($arr) . $type;
+        $namespace = implode('\\', $arr);
+        $dir = APP_PATH . '/gate/' . $type . '/' . implode('/', $arr);
+        $file = $class . '.php';
+
+        $template = "<?php
+namespace App\Gate\\$type\\%s;
+
+use App\Gate\\$type\\$type;
+
+class %s extends $type
+{
+
+}";
+        if (file_exists($dir . '/' . $file) && !$this->option('force')) {
+            echo Color::error('文件已存在！') . PHP_EOL;
+            return;
+        }
+        $data = sprintf($template, $namespace, $class);
+        file_put_contents($dir . '/' . $file, $data);
+        echo Color::success('文件新建成功！') . PHP_EOL;
     }
 
 }
